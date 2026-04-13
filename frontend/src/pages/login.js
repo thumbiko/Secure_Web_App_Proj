aba88db03a9302e0dfe7379e86e702d96b983df7
thumbiko nyasulu <thumbikonyasulu9@gmail.com>
2026-04-13 04:14:01 +0100

    pages setup

---
+++ frontend/src/pages/login.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import api from "../api/api";
+
+// login page for users
+// Sends credentials to backend and creates session
+
+export default function Login() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      const res = await api.post("/auth/login", {
+        email,
+        password
+      });
+
+      alert(res.data.msg);
+    } catch (err) {
+      alert("Login failed");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+
+      <input
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <input
+        placeholder="Password"
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button onClick={handleLogin}>
+        Login
+      </button>
+    </div>
+  );
+}
