7292eb32af29c5df815ef467e02d32f61f6d3d5c
thumbiko nyasulu <thumbikonyasulu9@gmail.com>
2026-04-13 04:48:37 +0100

    css add and app js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,15 +6,23 @@ import Home from "./pages/Home";
 import Bookings from "./pages/Bookings";
 import AdminDashboard from "./pages/AdminDashboard";
 
+// Main application routing system
+// This controls navigation between pages
+
 function App() {
   return (
     <Router>
       <Routes>
 
+        {/* Public pages */}
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+
+        {/* User feature */}
         <Route path="/bookings" element={<Bookings />} />
+
+        {/* Admin feature */}
         <Route path="/admin" element={<AdminDashboard />} />
 
       </Routes>
